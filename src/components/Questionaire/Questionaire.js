c425424dede524448401ee75fe551bb690018e52
idallas93 <Dallas.Isaac1@gmail.com>
2021-02-18 22:16:05 -0500

    shuffled multiple choice options so that they appear randomly and refactored homepage to be on questionnaire component

--- src/components/Questionaire/Questionaire.js
+++ src/components/Questionaire/Questionaire.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React from "react";
 
-const Questionaire = (props) => {
+const Button = ({ answer }) => {
+  return (
+    <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4">
+      {answer}
+    </button>
+  );
+};
 
-}
+const Questionaire = ({
+  handleAnswer,
+  data: { question, correct_answer, incorrect_answers },
+}) => {
+  const shuffledAnswer = [correct_answer, ...incorrect_answers].sort(
+    () => Math.random() - 0.5
+  );
+  return (
+    <div>
+      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
+        <h2
+          className="text-2xl"
+          dangerouslySetInnerHTML={{ __html: question }}
+        />
+      </div>
+      <div className="flex flex-wrap mt-4 justify-around">
+        <Button
+          className={correct_answer === shuffledAnswer[0] ? 'bg-green-300' : ''}
+          onClick={() => handleAnswer(shuffledAnswer[0])}
+          answer={shuffledAnswer[0]}
+        />
+        <Button
+         className={correct_answer === shuffledAnswer[1] ? 'bg-green-300' : ''}
+          onClick={() => handleAnswer(shuffledAnswer[1])}
+          answer={shuffledAnswer[1]}
+        />
+        <Button
+         className={correct_answer === shuffledAnswer[2] ? 'bg-green-300' : ''}
+          onClick={() => handleAnswer(shuffledAnswer[2])}
+          answer={shuffledAnswer[2]}
+        />
+        <Button
+         className={correct_answer === shuffledAnswer[3] ? 'bg-green-300' : ''}
+          onClick={() => handleAnswer(shuffledAnswer[3])}
+          answer={shuffledAnswer[3]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Questionaire;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 import "./style.css";
-import Button from "react-bootstrap/Button";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import { Questionaire } from '../../components/Questionaire'
 
 function HomePage() {
   const API_URL =
@@ -20,26 +18,12 @@ function HomePage() {
   }, []);
 
   // console.log("test log", questions[0].questions)
-
+  const handleAnswer = (answer) => {
+    // check for the answer
+  }
   return questions.length > 0 ? (
     <div className="container">
-      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
-        <h2 className="text-2xl"> {questions[0].question}</h2>
-      </div>
-      <div className="flex flex-wrap mt-4 justify-around">
-        <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4">
-          {questions[0].correct_answer}
-        </button>
-        <button className="bg-white w-2/5  p-4 text-purple-800 font-semibold rounded shadow mb-4">
-          {questions[0].incorrect_answers[0]}
-        </button>
-        <button className="bg-white w-2/5  p-4 text-purple-800 font-semibold rounded shadow">
-          {questions[0].incorrect_answers[1]}
-        </button>
-        <button className="bg-white w-2/5  p-4 text-purple-800 font-semibold rounded shadow">
-          {questions[0].incorrect_answers[2]}
-        </button>
-      </div>
+     <Questionaire data={questions[0]} handleAnswer={handleAnswer}/>
     </div>
   )  : (
     <h2 className='text-2xl text-white font-bold'>We are loading</h2>
