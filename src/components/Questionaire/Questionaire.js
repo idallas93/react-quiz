0a39cce34ceed99fba9fd5ed144b9b786a6daeb7
idallas93 <Dallas.Isaac1@gmail.com>
2021-02-20 11:32:26 -0500

    added handleAnswer function that cycles through questions on the page

--- src/components/Questionaire/Questionaire.js
+++ src/components/Questionaire/Questionaire.js
@@ -1,13 +1,5 @@
 import React from "react";
 
-const Button = ({ answer }) => {
-  return (
-    <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4">
-      {answer}
-    </button>
-  );
-};
-
 const Questionaire = ({
   handleAnswer,
   data: { question, correct_answer, incorrect_answers },
@@ -15,6 +7,8 @@ const Questionaire = ({
   const shuffledAnswer = [correct_answer, ...incorrect_answers].sort(
     () => Math.random() - 0.5
   );
+
+  console.log(correct_answer);
   return (
     <div>
       <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
@@ -24,26 +18,16 @@ const Questionaire = ({
         />
       </div>
       <div className="flex flex-wrap mt-4 justify-around">
-        <Button
-          className={correct_answer === shuffledAnswer[0] ? 'bg-green-300' : ''}
-          onClick={() => handleAnswer(shuffledAnswer[0])}
-          answer={shuffledAnswer[0]}
-        />
-        <Button
-         className={correct_answer === shuffledAnswer[1] ? 'bg-green-300' : ''}
-          onClick={() => handleAnswer(shuffledAnswer[1])}
-          answer={shuffledAnswer[1]}
-        />
-        <Button
-         className={correct_answer === shuffledAnswer[2] ? 'bg-green-300' : ''}
-          onClick={() => handleAnswer(shuffledAnswer[2])}
-          answer={shuffledAnswer[2]}
-        />
-        <Button
-         className={correct_answer === shuffledAnswer[3] ? 'bg-green-300' : ''}
-          onClick={() => handleAnswer(shuffledAnswer[3])}
-          answer={shuffledAnswer[3]}
-        />
+        {shuffledAnswer.map((answer) => (
+          <button
+            className={`${correct_answer 
+              === answer ? 'bg-purple-300' : 'bg-white'} p-4 
+              text-purple-800 font-semibold rounded shadow`}
+            onClick={() => handleAnswer(answer)}
+          >
+            {answer}
+          </button>
+        ))}
       </div>
     </div>
   );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,26 +7,37 @@ function HomePage() {
   const API_URL =
     "https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=multiple";
   const [questions, setQuestions] = useState([]);
-
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0)
+ 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         console.log(data.results)
         setQuestions(data.results);
+        console.log('index', data)
       });
   }, []);
 
   // console.log("test log", questions[0].questions)
   const handleAnswer = (answer) => {
+    setCurrentIndex(currentIndex + 1)
+
+    if(answer === questions[currentIndex].correct_answer) {
+      // increase the score 
+      setScore(score + 1)
+    }
     // check for the answer
+    // show another question
+    //change score if correct 
   }
   return questions.length > 0 ? (
     <div className="container">
-     <Questionaire data={questions[0]} handleAnswer={handleAnswer}/>
+     <Questionaire data={questions[currentIndex]} handleAnswer={handleAnswer}/>
     </div>
   )  : (
-    <h2 className='text-2xl text-white font-bold'>We are loading</h2>
+    <h2 className='text-2xl text-purple font-bold'>We are loading</h2>
   );
 }
 
