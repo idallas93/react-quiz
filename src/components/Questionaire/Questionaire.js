a50affd973e3fc81445c5139e30addb7bb406ae3
idallas93 <Dallas.Isaac1@gmail.com>
2021-02-20 14:06:51 -0500

    mvp quiz app funcitoning

--- src/components/Questionaire/Questionaire.js
+++ src/components/Questionaire/Questionaire.js
@@ -1,32 +1,47 @@
 import React from "react";
 
 const Questionaire = ({
+  showAnswers,
   handleAnswer,
+  handleNextQuestion,
   data: { question, correct_answer, incorrect_answers },
 }) => {
   const shuffledAnswer = [correct_answer, ...incorrect_answers].sort(
     () => Math.random() - 0.5
   );
-
-  console.log(correct_answer);
   return (
-    <div>
-      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
+    <div className='flex flex-col'>
+      <div className={`bg-white text-purple-800 p-10 rounded-lg shadow-md`}>
         <h2
           className="text-2xl"
           dangerouslySetInnerHTML={{ __html: question }}
         />
       </div>
-      <div className="flex flex-wrap mt-4 justify-around">
-        {shuffledAnswer.map((answer) => (
+      <div className="grid grid-cols2 gap-6 mt-6">
+        {shuffledAnswer.map((answer) => {
+          const textColor = showAnswers ? 
+          answer === correct_answer 
+          ? 'text-green-500'
+           : 'text-red-500' 
+           : 'text-purple-500';
+          return (
           <button
-            className='bg-white p-4 
-              text-purple-800 font-semibold rounded shadow'
-            onClick={() => handleAnswer(answer)}
+            className={`bg-white ${textColor} p-4 
+              font-semibold rounded shadow`}
+            onClick={() => handleAnswer
+              (answer)}
             dangerouslySetInnerHTML={{ __html: answer }}
           />
-        ))}
+          );
+        })}
       </div>
+      {showAnswers && (
+      <button 
+        onClick={handleNextQuestion}
+        className = {`ml-auto bg-purple-700 text-white p-4 font-semibold rounded shadow mt-6`}>
+          Next Question
+        </button>
+      )}
     </div>
   );
 };
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,13 +9,13 @@ function HomePage() {
   const [questions, setQuestions] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
   const [gameEnded, setGameEnded] = useState(false);
- 
+
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.results)
         setQuestions(data.results);
         console.log('index', data)
       });
@@ -23,26 +23,37 @@ function HomePage() {
 
   // console.log("test log", questions[0].questions)
   const handleAnswer = (answer) => {
-    const newIndex = currentIndex + 1
-    setCurrentIndex(newIndex);
-
+    // setCurrentIndex(newIndex);
+    if(!showAnswers) {
     if(answer === questions[currentIndex].correct_answer) {
       // increase the score 
       setScore(score + 1)
     }
+  }
+  setShowAnswers(true)
+    const newIndex = currentIndex + 1
 
     if(newIndex >= questions.length){
       setGameEnded(true);
     };
+
     // check for the answer
     // show another question
     //change score if correct 
-  }
+  };
+
+  const handleNextQuestion = () => {
+    setShowAnswers(false);
+
+    setCurrentIndex(currentIndex + 1)
+
+  };
+
   return gameEnded ? (
     <h1 className='text-3xl text-purple font-bold'> Your score was {score} </h1>
   ) : questions.length > 0 ? (
     <div className="container">
-     <Questionaire data={questions[currentIndex]} handleAnswer={handleAnswer}/>
+     <Questionaire data={questions[currentIndex]} showAnswers={showAnswers} handleNextQuestion ={handleNextQuestion} handleAnswer={handleAnswer}/>
     </div>
   )  : (
     <h2 className='text-2xl text-purple font-bold'>We are loading</h2>
